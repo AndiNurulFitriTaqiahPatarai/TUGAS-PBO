220575d1589b56eb97b73a577cf73e3e7ff4d048
Andi Nurul Fitri Taqiah Patarai <Andinurulfitri2403@gmail.com>
2025-03-03 21:17:07 +0800

    Add files via upload

---
+++ Tugas 1/src/Asgdll.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg13020220217_andinurulfitritaqiah;
+
+/**
+ *
+ * @author mac
+ */
+public class Asgdll {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      // TODO Auto-generated method stub 
+        /* Kamus */  
+        float f= 20.0f; 
+        double fll; 
+
+        /* Algoritma */  
+        fll=10.0f; 
+        System.out.println ("f  : "+f  + 
+       "\nf11: "+fll); 
+    }
+    
+}
